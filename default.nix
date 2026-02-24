aeda8659166afd8112ce525d5cb460bda514265a
ivanw-cronos <ivan.sl.wong@cronoslabs.org>
2026-02-24 16:50:37 +0800

    Update cargo hash

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
   }:
     pkgs.rustPlatform.buildRustPackage {
       inherit pname cargoFlags;
-      cargoHash = "";
+      cargoHash = "sha256-4LIKiCVLp5PMAkEdhPZiyKczAgR9Cfkw8mJv05yAWH4=";
       src = ./.;
       version = "0.1.0";
     };
